4b4eb07292b89c6afb13d41d89ef5ffb616411b7
benya7 <bitup.games@gmail.com>
2025-02-25 17:00:05 +0100

    fix: Alert component styles

--- frontend/src/components/Alert.tsx
+++ frontend/src/components/Alert.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 export function Alert({ destructive = false, title, description }: Props) {
     return (
-        <AlertContainer className="my-2" variant={destructive ? 'destructive' : 'default'}>
+        <AlertContainer className="my-2 bg-background w-96 min-h-20 max-h-96 shadow overflow-hidden text-ellipsis" variant={destructive ? 'destructive' : 'default'}>
             <AlertCircle className="h-4 w-4" />
             <AlertTitle>{title}</AlertTitle>
             <AlertDescription>{description}</AlertDescription>
